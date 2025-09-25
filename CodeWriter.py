0a8922e5a7aa549fea0faaff891baa6dc0d624b8
David Scott <dscott764@proton.me>
2025-09-25 15:51:18 -0700

    Passed StackTest.tst

--- CodeWriter.py
+++ CodeWriter.py
@@ -7,22 +7,160 @@ class CodeWriter:
         Opens the output file for writing and prepares for code generation.
         """
         self.output_file = open(output_file_path, 'w')
+        self.label_counter = 0  # To generate unique labels
+
+    def _write_binary_op(self, operation):
+        """Helper to write code for binary operations."""
+        return [
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            operation,  # The only line that changes
+            '@SP',
+            'M=M+1'
+        ]
 
     def write_arithmetic(self, command):
         """Writes the assembly code for the given arithmetic command."""
         assembly_code = []
         if command == 'add':
+            assembly_code.extend(self._write_binary_op('M=D+M'))
+        elif command == 'sub':
+            assembly_code.extend(self._write_binary_op('M=M-D'))
+        elif command == 'and':
+            assembly_code.extend(self._write_binary_op('M=D&M'))
+        elif command == 'or':
+            assembly_code.extend(self._write_binary_op('M=D|M'))
+        elif command == 'eq':
+            # Generate unique labels for this comparison
+            true_label = f'EQ_TRUE_{self.label_counter}'
+            end_label = f'EQ_END_{self.label_counter}'
+            self.label_counter += 1
+
             assembly_code.extend([
-                '// add',
-                '@SP',      # Decrement stack pointer
+                f'// eq',
+                '@SP',
+                'M=M-1',    # Decrement SP
+                'A=M',
+                'D=M',      # D = y (first value)
+                '@SP',
+                'M=M-1',    # Decrement SP
+                'A=M',
+                'D=M-D',    # D = x - y
+                f'@{true_label}',
+                'D;JEQ',    # If x == y (D is zero), jump to TRUE case
+
+                # False case: set D to 0
+                'D=0',
+                f'@{end_label}',
+                '0;JMP',    # Unconditionally jump to the end
+
+                # True case: set D to -1
+                f'({true_label})',
+                'D=-1',
+
+                # Push the result from D onto the stack
+                f'({end_label})',
+                '@SP',
+                'A=M',
+                'M=D',      # *SP = D (which is 0 or -1)
+                '@SP',
+                'M=M+1'     # SP++
+            ])
+        elif command == 'lt':
+            # Generate unique labels for this comparison
+            true_label = f'LT_TRUE_{self.label_counter}'
+            end_label = f'LT_END_{self.label_counter}'
+            self.label_counter += 1
+
+            assembly_code.extend([
+                f'// lt',
+                '@SP',
                 'M=M-1',
-                'A=M',      # Get address of y (top value)
-                'D=M',      # Store y in D register
-                '@SP',      # Decrement stack pointer again
+                'A=M',
+                'D=M',      # D = y
+                '@SP',
                 'M=M-1',
-                'A=M',      # Get address of x (new top value)
-                'M=D+M',    # Store x+y in M (at address of x)
-                '@SP',      # Increment stack pointer
+                'A=M',
+                'D=M-D',    # D = x - y
+                f'@{true_label}',
+                'D;JLT',    # If x < y (D is negative), jump to TRUE
+
+                # False case: set D to 0
+                'D=0',
+                f'@{end_label}',
+                '0;JMP',
+
+                # True case: set D to -1
+                f'({true_label})',
+                'D=-1',
+
+                # Push the result from D onto the stack
+                f'({end_label})',
+                '@SP',
+                'A=M',
+                'M=D',
+                '@SP',
+                'M=M+1'
+            ])
+        elif command == 'gt':
+            # Generate unique labels for this comparison
+            true_label = f'GT_TRUE_{self.label_counter}'
+            end_label = f'GT_END_{self.label_counter}'
+            self.label_counter += 1
+
+            assembly_code.extend([
+                f'// gt',
+                '@SP',
+                'M=M-1',
+                'A=M',
+                'D=M',      # D = y
+                '@SP',
+                'M=M-1',
+                'A=M',
+                'D=M-D',    # D = x - y
+                f'@{true_label}',
+                'D;JGT',    # If x > y (D is positive), jump to TRUE
+
+                # False case: set D to 0
+                'D=0',
+                f'@{end_label}',
+                '0;JMP',
+
+                # True case: set D to -1
+                f'({true_label})',
+                'D=-1',
+
+                # Push the result from D onto the stack
+                f'({end_label})',
+                '@SP',
+                'A=M',
+                'M=D',
+                '@SP',
+                'M=M+1'
+            ])
+        elif command == 'neg':
+            assembly_code.extend([
+                '// neg',
+                '@SP',
+                'M=M-1',    # Point to the top of the stack
+                'A=M',      # Get the address
+                'M=-M',     # Negate the value in place
+                '@SP',
+                'M=M+1'     # Increment the stack pointer
+            ])
+        elif command == 'not':
+            assembly_code.extend([
+                '// not',
+                '@SP',
+                'M=M-1',
+                'A=M',
+                'M=!M',     # Perform bitwise NOT on the value in place
+                '@SP',
                 'M=M+1'
             ])
 
